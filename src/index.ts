2155ee04077de13b3a5051f11cc362f6e7bc8411
Una <una.lestrange@atu.ie>
2025-09-22 11:00:55 +0100

    added .env for port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express, {Application, Request, Response} from "express" ;
 import morgan from "morgan";
 import userRoutes from './routes/users';
+import dotenv from 'dotenv';
 
 
-const PORT = process.env.PORT || 3000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3001;
+
 
 const app: Application = express();
 
